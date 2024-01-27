047c2d63419a53d8030b2a1b2d9392acda8431ee
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-01-27 06:46:06 -0800

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,70 @@
+namespace CS161_Practice4
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void TestScoreButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void payrollOvertimeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loanQualifierButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fuelEconomyButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorThemeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeZoneButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void romanNumeralButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void massWeightButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void magicDateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorMixerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void distanceConverterButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bookClubButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
